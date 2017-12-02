c36500582dbc327fd8c2d50b4a55f83e27f1529f
piers7 <male@piers7.com>
2017-12-02 14:29:05 +0000

    Add -y for apt-get call

--- src/setupBuild.sh
+++ src/setupBuild.sh
@@ -1,8 +1,11 @@
-sudo apt-get install scons mono-complete 
+echo "Installing pre-reqs"
+sudo apt-get install scons mono-complete nuget -y
 
-# setup trusted roots, or nuget package restore won't work
+echo "Setup trusted roots, or nuget package restore won't work"
 # see http://stackoverflow.com/questions/15181888/nuget-on-linux-error-getting-response-stream
-sudo mozroots --import --machine --sync
+# sudo mozroots --import --machine --sync
+# Updated: https://stackoverflow.com/a/30970569/26167
+sudo mozroots --import --machine --sync --url https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt
 
 # following need 'Y's sent to them - maybe simplest as a manual step
 sudo certmgr -ssl -m https://go.microsoft.com
